637e438dfc2563b24e18c78373ece1de3a234135
matsumura <matsumura@computer>
2011-11-25 22:28:01 +0900

    2回目のコミット

--- src/test/HelloWorldTest01.java
+++ src/test/HelloWorldTest01.java
@@ -3,9 +3,14 @@ package test;
 public class HelloWorldTest01 {
 	public static void main(String[] args) {
 		new HelloWorldTest01().execute();
+		new HelloWorldTest01().execute2();
 	}
 
 	private void execute() {
 		System.out.println("Hello Git!");
+	}
+
+	private void execute2() {
+		System.out.println("Hello Git2!");
 	}
 }
