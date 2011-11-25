84d263e1fb89b0a2d0129db7f906f57dabf744e1
mattun0x0 <mattun0x0@gmail.com>
2011-11-25 22:12:51 +0900

    test

---
+++ src/test/HelloWorldTest01.java
@@ -0,0 +1,11 @@
+package test;
+
+public class HelloWorldTest01 {
+	public static void main(String[] args) {
+		new HelloWorldTest01().execute();
+	}
+
+	private void execute() {
+		System.out.println("Hello Git!");
+	}
+}
